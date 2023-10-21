de7db7737be6c6040d393318cfd2e40fd5d03bb9
mchhan <rndmmiscthings@gmail.com>
2023-10-20 20:04:37 -0700

    Add function to play one round
    Modify player input and computer input to return correct round outcome.

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 //Get computer output//
 function getComputerChoice() {
     //Computer randomly chooses between 3 values//
-    let computerChoice = Math.random() * (3 - 1) + 1;
-    let computerHand = Math.round(computerChoice);
+    const randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1);
     //The 3 values correspond to a different hand//
-    if (computerHand === 1) {
-        return "Computer plays Rock!";
-    } else if (computerHand === 2) {
-        return "Computer plays Paper!";
-    } else {
-        return "Computer plays Scissors!";
+    if (randomNumber === 1) {
+        computerChoice = "Rock";
+    } else if (randomNumber === 2) {
+        computerChoice = "Paper";
+    } else if (randomNumber === 3) {
+        computerChoice = "Scissors";
     }
+    return computerChoice;
 }
 
 console.log(getComputerChoice());
+//PROBLEM WITH COMPUTERSELECTION AND PLAYROUND//
 
-//Single round of Rock-Paper-Scissors and has two parameters- playerSelection and computerSelection//
-function singleRound() {
-    //playerSelection parameter gets user input for hand played//
-    function playerSelection() {
-        let playerSelection = prompt("What hand do you want to play?");
-        let playerHand = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
-        console.log(playerHand);
-    }
-    
+//Compare hands played and give result//
+function playRound(playerSelection, computerSelection) { 
+    //If player chooses rock//
+    if (playerSelection === "Rock" && computerSelection === "Rock") {
+        return "Tie. Try again."
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You lose. Paper beats Rock.";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You win. Rock bests Scissors.";
+    //If player chooses paper//
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        return "Tie. Try again.";
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You lose. Scissors beats Paper.";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win. Paper beats Rock.";
+    //If player chooses scissors//
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        return "Tie. Try again.";
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You lose. Rock beats Scissors.";
+    } else (playerSelection === "Scissors" && computerSelection === "Paper")
+        return "You win. Scissors beats Paper.";
 }
+
+//Get player input//
+let playerChoice = prompt("What hand do you want to play?");
+const playerSelection = playerChoice.charAt(0).toUpperCase() + playerChoice.toLowerCase().slice(1);
+
+console.log(playerSelection); 
+
+
+const computerSelection = computerChoice;
+console.log(playRound(playerSelection, computerSelection));
